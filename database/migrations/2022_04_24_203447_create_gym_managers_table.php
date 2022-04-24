d1ad41686a0db8ef0eec4e6545e197f7dccd2cee
AbdElrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-04-24 22:39:35 +0200

    added gym manager

---
+++ database/migrations/2022_04_24_203447_create_gym_managers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gym_managers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->integer('national_id')->unique();
+            $table->string('avatar_image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gym_managers');
+    }
+};
