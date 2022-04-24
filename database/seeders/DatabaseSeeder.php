68bc99c682a9b258a9090ad5330ebadbcc531231
AbdElrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-04-24 21:43:46 +0200

    seed super admin completed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // create super admin user
+        \App\Models\User::factory(1)->create();
     }
 }
